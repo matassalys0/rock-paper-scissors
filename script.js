62866aade8030a3909629510dc8359a28fecf3ad
matassalys0 <matassalys@gmail.com>
2022-01-31 20:22:07 +0200

    added a 5 round gameplay loop

--- script.js
+++ script.js
@@ -18,36 +18,82 @@ function computerPlay() {
     }
 }
 
-function checkIfValidSelection(playerSelection) {
+function checkIfValidSelection(playerSelection) 
+{
     if (playerSelection === "Scissors" || playerSelection === "Rock" || playerSelection === "Paper")
         return true;
     else
         return false;
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection)
+{
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
 
     if(!checkIfValidSelection(playerSelection))
-        return "Wrong selection! You can only choose rock paper or scissors!";
+        return "lose";
 
     console.log(`Computer plays ${computerSelection}`);
     if(playerSelection === computerSelection)
-        return "It's a tie! Play again."
+        return "tie"
     else if ((playerSelection === "Scissors" && computerSelection === "Paper") || 
     (playerSelection === "Paper" && computerSelection === "Rock") || 
     (playerSelection === "Rock" && computerSelection === "Scissors")) {
-        return "You win! Congrats!"
+        return "win"
     }
     else {
-        return "You lose :(. Try again."
+        return "lose"
     }
     
 }
 
-let playerSelection = prompt('Duel! Rock, paper or scissors!?');
+function game() 
+{
+    let playerScore = 0;
+    let compScore = 0;
 
-console.log(playRound(playerSelection, computerPlay()));
+    for(let i = 0; i <= 4; i++) 
+    {
+       console.log(`Round ${i + 1}, let's go!`) 
+       console.log(`Current score: You - ${playerScore}, computer - ${compScore}`);
+       let playerSelection = prompt('Duel! Rock, paper or scissors!? If you put something else you lose automatically!');
+       let result = playRound(playerSelection, computerPlay());
+
+       switch(result) {
+           case "win": 
+                console.log("You win!");
+                playerScore++;
+                break;
+
+           case "lose":
+                console.log("You lose!");
+                compScore++;
+                break;
+
+           default:
+                console.log("It's a tie!");
+       } 
+    }
+
+    if(playerScore > compScore) 
+    {
+        console.log("End of the game! You win!");
+        console.log(`Final score: You - ${playerScore}, computer - ${compScore}`);
+    }
+    else if (compScore > playerScore)
+    {
+        console.log("End of the game! You lose!");
+        console.log(`Final score: You - ${playerScore}, computer - ${compScore}`);
+    } 
+    else 
+    {
+        console.log("End of the game! It's a tie!");
+        console.log(`Final score: You - ${playerScore}, computer - ${compScore}`);
+    }
+        
+}
+
+ game();
 
 
